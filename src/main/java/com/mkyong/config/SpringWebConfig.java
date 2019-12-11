673499df31ba8450483e5aadadabcedcbd5b212e
rahulrsavant <Vidula@9975>
2019-12-11 21:38:15 +0530

    //9975 line no 12

--- src/main/java/com/mkyong/config/SpringWebConfig.java
+++ src/main/java/com/mkyong/config/SpringWebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
- 
+ //9975
 @EnableWebMvc
 @Configuration
 @ComponentScan({ "com.mkyong.web" })
